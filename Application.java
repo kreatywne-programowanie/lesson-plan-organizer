b9595ccd7c8f7be5fea0baed6336baea7984fae1
Tomasz Marciniuk <marciniuktomasz@interia.pl>
2019-10-18 21:09:46 +0200

    zmiana w aplikacji

--- Application.java
+++ Application.java
@@ -7,5 +7,7 @@ public class Application {
         Lesson lesson = new Lesson(1, "Abc");
         System.out.println(lesson);
         System.out.println("Witaj");
+        Group group = new Group("robotyka", "3", "Tomek","sala 3");
+        System.out.println(group);
     }
 }
