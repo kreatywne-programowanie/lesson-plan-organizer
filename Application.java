8d881d5bead09eee1ea6d2091e56d7de4dd2e5ce
cokolwiek <cokolwiek@wp.pl>
2019-10-18 21:06:26 +0200

    byle co

--- Application.java
+++ Application.java
@@ -5,7 +5,10 @@ public class Application {
 
     public static void main(String[] args) {
         Lesson lesson = new Lesson(1, "Abc");
+        Teacher teacher = new Teacher ("Jan ", "Kowalski ", "robotyka");
         System.out.println(lesson);
         System.out.println("Witaj");
+        System.out.println(" Twój nauczyciel to: ");
+        System.out.println(teacher);
     }
 }
