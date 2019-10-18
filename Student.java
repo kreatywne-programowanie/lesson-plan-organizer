53f5ed5fb891a9a41746b3719311741e68236cb0
Piotr Marciniuk <marciniukpiotr@wp.pl>
2019-10-18 21:20:30 +0200

    test

--- Student.java
+++ Student.java
@@ -3,19 +3,22 @@
  */
 public class Student {
 
-    private String name, surname, teacher;
     private int age;
+    private String name;
+    private String surname;
+    private String teacher;
+    private String interests;
 
-    public Student (int age, String name,surname,teacher,interests) {
+    public Student(int age, String name, String surname, String teacher, String interests) {
         this.age = age;
-        this.teacher = teacher;
-        this.name = name;
-        this.surname = surname;
+        this.teacher = name;
+        this.name = surname;
+        this.surname = teacher;
         this.interests = interests;
     }
 
     @Override
     public String toString() {
-        return age + " " + teacher + " " + name + " " + surname + "interests";
+        return (age + name + " " + surname + " " + teacher + " " + interests);
     }
 }
